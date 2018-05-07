706b66bb90c02310d13af667d8cbc05a27085a82
Sabine Gruber <sab@vollbio.de>
2018-05-07 10:15:17 +0200

    intermediate exercise

--- manipulating_html_page_exercise_files/myscript.js
+++ manipulating_html_page_exercise_files/myscript.js
@@ -1,8 +1,15 @@
-$("form").submit(function(e) {
+$(document).ready(function(){
+// $("form").submit(function(e) {
 
-	e.preventDefault();
-	
-	
-	
+// 	e.preventDefault();
+// });
+//$("form").remove();
 
+$("article").prepend("<div>1</div>", "<div>2</div>", "<div>3</div>", "<div>4</div>", "<div>5</div>");
+
+$("input").attr("value", "search for...");
+
+$("div").addClass("box");
+
+$("a[href^='http://www.codefactory.']").attr("href", "http://www.codefactory.wien");
 });
